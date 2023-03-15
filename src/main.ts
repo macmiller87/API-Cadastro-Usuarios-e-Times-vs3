e0b9c82d3a0497c6c398c4f1fbf98d00a37ce37e
macmiller87 <macamagolf@gmail.com>
2023-03-15 15:04:30 -0300

    build: arquivo de construcao base projeto nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './infra/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(8080, () =>
+    console.log('Server is running at port 8080 🔥🚀'),
+  );
 }
 bootstrap();
