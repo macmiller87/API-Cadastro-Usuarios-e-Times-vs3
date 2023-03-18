e2dfe55b0efd395dcfdccd115b99e56d7dc80cfa
macmiller87 <macamagolf@gmail.com>
2023-03-18 18:09:04 -0300

    Refactor: criacao da tabela migration

---
+++ prisma/migrations/20230318205223_create_users_and_teams_and_user_token/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "user_id" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "userAvatar" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "Teams" (
+    "team_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "TeamName" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Teams_pkey" PRIMARY KEY ("team_id")
+);
+
+-- CreateTable
+CREATE TABLE "UserToken" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_userName_key" ON "Users"("userName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Teams_TeamName_key" ON "Teams"("TeamName");
+
+-- AddForeignKey
+ALTER TABLE "Teams" ADD CONSTRAINT "Teams_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserToken" ADD CONSTRAINT "UserToken_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
