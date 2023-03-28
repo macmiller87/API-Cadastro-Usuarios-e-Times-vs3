52020cc904f569d47d993e13cdd481b8b9992eed
macmiller87 <macamagolf@gmail.com>
2023-03-28 19:02:49 -0300

    Refactor: remove coments EnsureUserAuthenticate

--- src/utils/auth/EnsureUserAuthenticate.ts
+++ src/utils/auth/EnsureUserAuthenticate.ts
@@ -33,29 +33,3 @@ export class EnsureUserAuthenticate implements NestMiddleware {
     }
   }
 }
-
-
-// eslint-disable-next-line prettier/prettier
-// export async function ensureUserAuthenticate(request: Request, response: Response, next: NextFunction) {
-
-//   try {
-//     const authHeader = request.headers.authorization;
-
-//     if (!authHeader) {
-//       throw new AppError('JWT Token is Missing !', 401);
-//     }
-
-//     const [, token] = authHeader.split(' ');
-
-//     /* eslint-disable prettier/prettier */
-//     const { sub: user_id } = verify(token, AuthConfig.jwt.secret_token) as IPayload;
-
-//     request.user = {
-//       user_id: user_id,
-//     };
-
-//     next();
-//   } catch {
-//     throw new AppError('Invalid Token', 400);
-//   }
-// }
