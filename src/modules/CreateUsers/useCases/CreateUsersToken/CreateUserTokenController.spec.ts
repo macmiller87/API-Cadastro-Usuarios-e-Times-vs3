0eb9be368895e10452d906f2fbeb2e43736d0a75
macmiller87 <macamagolf@gmail.com>
2023-03-29 16:09:37 -0300

    Refactor: tests End to end createUserToken

--- src/modules/CreateUsers/useCases/CreateUsersToken/CreateUserTokenController.spec.ts
+++ src/modules/CreateUsers/useCases/CreateUsersToken/CreateUserTokenController.spec.ts
@@ -2,43 +2,33 @@
 import 'dotenv/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '@database/prisma/prisma.service';
-import { UserController } from '@controller/User.controller';
-import { CreateUsers } from '@modules/CreateUsers/useCases/CreateUsers/CreateUsers';
-import { AuthenticateUsersToken } from './CreateUsersToken';
-import { UserDatabaseModule } from '@database/user_database.module';
-import { UserTokenDatabaseModule } from '@database/userToken-database.module';
-import { ListSpecifcUser } from '@modules/CreateUsers/useCases/ListUser/ListSpecifcUser';
-import { ListUsersAndTeams } from '@modules/CreateUsers/useCases/ListUsersAndTeams/ListUsersAndTeams';
-import { DeleteUsers } from '@modules/CreateUsers/useCases/DeleteUsers/DeleteUsers';
-import { AppError } from '@utils/errors/AppError';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../../../../infra/app.module';
 
 let prismaService: PrismaService;
-let userController: UserController;
-let createUser: CreateUsers;
-let createUserToken: AuthenticateUsersToken;
+let appModule: AppModule;
 
+let app: INestApplication;
 describe('Create a UserToken, (Tests End to End)', () => {
-  beforeEach(async () => {
+  
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UserDatabaseModule, UserTokenDatabaseModule],
-      controllers: [UserController],
-      providers: [
-        CreateUsers,
-        ListSpecifcUser,
-        ListUsersAndTeams,
-        DeleteUsers,
-        AuthenticateUsersToken,
-      ],
+      imports: [AppModule],
     }).compile();
 
     prismaService = module.get<PrismaService>(PrismaService);
-    userController = module.get<UserController>(UserController);
-    createUser = module.get<CreateUsers>(CreateUsers);
-    createUserToken = module.get<AuthenticateUsersToken>(AuthenticateUsersToken);
+    appModule = module.get<AppModule>(AppModule);
+
+    app = module.createNestApplication({
+      logger: false,
+    });
+
+    await app.init();
   });
 
   it('Controller should be defined', () => {
-    expect(userController).toBeDefined();
+    expect(appModule).toBeDefined();
   });
 
   afterAll(async () => {
@@ -48,53 +38,54 @@ describe('Create a UserToken, (Tests End to End)', () => {
   });
 
   it('Should be able to create a UserToken, if user already exists, and email and password is correct!', async () => {
-    const user = await createUser.execute({
+    const user = await request(app.getHttpServer()).post('/users').send({
       userName: 'Madruga',
       userAvatar: 'Madruguinha',
       email: 'madruguinha@gmail.com',
       password: '7945',
     });
 
-    const userTokenAuthenthicate = await createUserToken.execute({
-      email: user.user.email,
+    const userTokenAuthenthicate = await request(app.getHttpServer()).post('/users/userToken').send({
+      email: user.body.userProps.email,
       password: '7945',
     });
 
     expect(201);
-    expect(userTokenAuthenthicate.user).toHaveProperty('userName', 'Madruga');
-    expect(userTokenAuthenthicate.user).toHaveProperty('email', 'madruguinha@gmail.com');
-    expect(userTokenAuthenthicate).toHaveProperty('token');
+    expect(userTokenAuthenthicate.body.user).toHaveProperty('userName', 'Madruga');
+    expect(userTokenAuthenthicate.body.user).toHaveProperty('email', 'madruguinha@gmail.com');
+    expect(userTokenAuthenthicate.body).toHaveProperty('token');
   });
 
   it('Should not be able to Create a UserToken, if user non exists, or email is incorrect!', async () => {
-    await createUser.execute({
+    const user = await request(app.getHttpServer()).post('/users').send({
       userName: 'Mary Tucker',
       userAvatar: 'Tucker',
       email: 'forke@velru.be',
       password: '3443',
     });
 
-    await expect(
-       createUserToken.execute({
-        email: 'Fake Email',
-        password: '3443',
-      }),
-    ).rejects.toEqual(new AppError('User Email Not Found or Incorrect !', 404));
+    const userToken =  await request(app.getHttpServer()).post('/users/userToken').send({
+      email: 'Fake Email',
+      password: user.body.userProps.password,
+    });
+
+    expect(userToken.body).toStrictEqual({ statusCode: 404, message: 'User Email Not Found or Incorrect !' });
   });
 
   it('Should not be able to Create a UserToken, if user non exists, or password is incorrect !', async () => {
-    const user = await createUser.execute({
+    const user = await request(app.getHttpServer()).post('/users').send({
       userName: 'Blanche Kim',
       userAvatar: 'Kim',
       email: 'pasrujuh@ofapav.ne',
       password: '3945',
     });
 
-    await expect(
-      createUserToken.execute({
-       email: user.user.email,
-       password: 'Fake Password',
-     }),
-   ).rejects.toEqual(new AppError('Password Incorrect !', 404));
+    const userToken = await request(app.getHttpServer()).post('/users/userToken').send({
+      email: user.body.userProps.email,
+      password: 'Fake Password',
+    });
+
+    expect(userToken.body).toStrictEqual({ statusCode: 404, message: 'Password Incorrect !' });
   });
+
 });
